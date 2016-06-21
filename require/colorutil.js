296d83d1607184ee6fa179f391b9c78f4c9d3ab7
Student <Student@Students-Mac-mini.local>
2016-06-21 16:49:09 -0600

    completed bonus 1 in node colors exercise

--- require/colorutil.js
+++ require/colorutil.js
@@ -1,13 +1,22 @@
-function part1(){
 var lumin1 = process.argv[2]
 var lumin2 = process.argv[3]
 var lumin3 = process.argv[4]
 
-var luminosity = 0.2126*lumin1 + 0.7152*lumin2 + 0.0722*lumin3
+function part1(){
+  var luminosity = 0.2126*lumin1 + 0.7152*lumin2 + 0.0722*lumin3
 
-console.log(luminosity)
+    console.log(luminosity)
+}
+
+function bonus1(){
+  var r = lumin1*.8
+  var g = lumin2*.8
+  var b = lumin3*.8
+
+    console.log(r, g, b)
 }
 
 module.exports = {
-  part1 : part1
+  part1 : part1,
+  bonus1 : bonus1
 }
---
+++ require/darken-cli.js
@@ -0,0 +1,8 @@
+var colorutil = require('./colorutil.js')
+
+console.log(colorutil.bonus1())
+
+// Students-Mac-mini:require Student$ node darken-cli.js 255 239 213
+// 204 191.20000000000002 170.4
+// Students-Mac-mini:require Student$ node darken-cli.js 50 15 25
+// 40 12 20
